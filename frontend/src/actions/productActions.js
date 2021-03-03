9b80302eb479337f7aad2baeeda82f3931154fc1
Manjunath Gowdar <57346550+Manjunath-Gowdar-au9@users.noreply.github.com>
2021-03-03 20:04:27 +0530

    productConstants, productReducer, productActions added

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,27 @@
+import {
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_REQUEST,
+} from '../constants/productConstants.js'
+import axios from 'axios'
+
+export const listProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST })
+
+    //the data will be used in product list success
+    const { data } = await axios.cen('/api/products')
+    dispatch({
+      type: PRODUCT_LIST_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
