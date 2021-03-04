8b82ca8cff44639fb15aa67daa64ff4e761178e9
Manjunath Gowdar <57346550+Manjunath-Gowdar-au9@users.noreply.github.com>
2021-03-04 22:30:24 +0530

    action and reducer for product details page

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,21 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer } from './reducers/productReducers.js'
+import {
+  productListReducer,
+  productDetailsReducer,
+} from './reducers/productReducers.js'
 
 const reducer = combineReducers({
-    productList:productListReducer,   //productList is the state
+  productList: productListReducer, //productList is one of the state's data
+  productDetails: productDetailsReducer, //productList is one of the state's data
 })
 const initialState = {}
 const middleware = [thunk]
-const store = createStore(reducer,initialState,composeWithDevTools(applyMiddleware(...middleware)))
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
 
 export default store
